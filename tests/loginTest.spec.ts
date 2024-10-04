815c73d8719d66686972130409d4071d6edea0c2
Jabes Valdepeñas <jvaldepenas@outlook.com>
2024-10-04 17:30:11 -0600

    changed the test name

--- tests/loginTest.spec.ts
+++ tests/loginTest.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('login standard user', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
   await expect(page.locator('#root')).toContainText('Swag Labs');
   await expect(page.locator('[data-test="login-button"]')).toContainText('Login');
